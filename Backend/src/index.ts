5ff91a545767b4c0f5d64ea8a0ad6b58d6fa3ab9
dineshgurung123 <134469961+dineshgurung123@users.noreply.github.com>
2025-04-26 19:09:01 +0545

    cors error

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://tasty-bites-ten.vercel.app/"],
+    origin: ["http://localhost:5173", "https://tasty-bites-ten.vercel.app"],
     credentials: true,
   })
 );
