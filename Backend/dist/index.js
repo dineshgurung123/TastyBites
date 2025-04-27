c3b96bd70b26364f08ee1a3ed72d23dd9d2a3aeb
dineshgurung123 <134469961+dineshgurung123@users.noreply.github.com>
2025-04-27 15:12:41 +0545

    cors error solved

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -12,23 +12,27 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const cartRoutes_1 = __importDefault(require("./routes/cartRoutes"));
 const ordeRoutes_1 = __importDefault(require("./routes/ordeRoutes"));
 const cors_1 = __importDefault(require("cors"));
-//Database connection
+// Database connection
 (0, config_1.default)();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3001;
 dotenv_1.default.config();
-//Middleware
+// Middleware
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+    origin: [
+        "https://tasty-bites-ten.vercel.app",
+        "http://localhost:5173",
+    ],
     credentials: true,
 }));
-//Routes
+// Routes
 app.use("/api/auths", authRoutes_1.default);
 app.use("/api/foods", foodRoutes_1.default);
 app.use("/api/carts", cartRoutes_1.default);
 app.use("/api/orders", ordeRoutes_1.default);
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
